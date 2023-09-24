4a59930f3aad1299406b3950812b9f45d5500178
570n3R <81666116+570n3R@users.noreply.github.com>
2023-09-24 18:21:36 +0530

    Update comingsoon.js

--- assets/js/comingsoon.js
+++ assets/js/comingsoon.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("Sep 25, 2023 00:00:00").getTime();
+    const countDate = new Date("Oct 25, 2023 00:00:00").getTime();
 
     const currentTime = new Date().getTime();
     console.log(currentTime);
